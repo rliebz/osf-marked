f64afd3808baeb2f07d0ee4bf1e5c71a6200f48b
Chris Seto <chriskseto@gmail.com>
2014-08-12 13:15:47 -0400

    Use Sockjs rather than m.request

--- wiki/static/js/wiki.js
+++ wiki/static/js/wiki.js
@@ -1,21 +1,53 @@
+(function() {
+    var self = {};
 
+    self.socket =  new SockJS('/sock');
+    self.cbs = [];
+
+    self.getPage = function(title, cb) {
+        options = {
+            method: 'GET',
+            page: title
+        }
+        self.cbs.unshift(cb);
+        return self.socket.send(JSON.stringify(options));
+    };
+
+    self.savePage = function(title, text) {
+        options = {
+            method: 'PUT',
+            page: title,
+            content: text
+        }
+        return self.socket.send(JSON.stringify(options));
+    }
+
+    self.socket.onopen = function() {
+    };
+
+    self.socket.onmessage = function(e) {
+        cb = self.cbs.pop();
+        if (cb !== undefined) cb(e.data);
+        console.log(e);
+
+    };
+
+    self.socket.onclose = function() {
+
+    };
+
+    window.w = self;
+})();
 
 var wiki = {
     Page: function(title, cb) {
         var self = this;
         self.title = title;
-        m.request({
-            method: 'GET',
-            url: '/api/v1/page/' + title,
-            unwrapSuccess: function(response) {
-                self.initText = response.content;
-                cb(self);
-            },
-            unwrapFailure: function(response) {
-                self.initText = '';
-                cb(self);
-            }
-        });
+        self.fetched = function(data) {
+            self.initText = data.content;
+            cb(self);
+        }
+        w.getPage(title, self.fetched);
     },
 
     controller: function() {
@@ -26,14 +58,7 @@ var wiki = {
         self.editor = new aceEditor.controller();
 
         self.save = function() {
-            m.request({
-                method: 'PUT',
-                url: '/api/v1/page/' + self.requestedPage(),
-                data: {
-                    content: self.editor.getText().split('\n')
-                }
-            });
-
+            w.savePage(self.requestedPage(), self.editor.getText().split('\n'));
         }.bind(self);
 
         self.load = function() {
