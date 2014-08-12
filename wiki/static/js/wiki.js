f3503babf831b4c04f37934d15e426f810ce2908
Chris Seto <chriskseto@gmail.com>
2014-08-12 11:21:20 -0400

    Basic prototype now works still a bit buggy

--- wiki/static/js/wiki.js
+++ wiki/static/js/wiki.js
@@ -1,48 +1,56 @@
-var aceEditor = {
-    controller: function() {
-        var self = this;
-        self.editor = null;
 
-        self.config = function(element, isInitialized) {
-            if(isInitialized) return;
-            self.editor = ace.edit(element.id);
-            self.editor.on('change', m.redraw);
-            self.editor.getSession().setMode("ace/mode/markdown");
-        };
-
-        self.getText = function() {
-            if (self.editor === null) return '';
-            return self.editor.getSession().getValue();
-        };
-
-        self.setText = function(text) {
-            if (self.editor === null) return '';
-            return self.editor.getSession().setValue(text);
-        };
-
-        self.markUp = function(){
-            return m.trust(marked(self.getText()));
-        }
-    },
-    view: function(ctrl) {
-        return m('#aceEditor', [
-            m('#rendered', ctrl.markUp()),
-            m('#editor', {config: ctrl.config}),
-        ]);
-    }
-}
 
 var wiki = {
+    Page: function(title, cb) {
+        var self = this;
+        self.title = title;
+        m.request({
+            method: 'GET',
+            url: '/api/v1/page/' + title,
+            unwrapSuccess: function(response) {
+                self.initText = response.content;
+                cb(self);
+            },
+            unwrapFailure: function(response) {
+                self.initText = '';
+                cb(self);
+            }
+        });
+    },
+
     controller: function() {
         var self = this;
+
+        self.requestedPage = m.prop('')
+        self.currentPage =  null; //new wiki.Page('test');
         self.editor = new aceEditor.controller();
 
         self.save = function() {
+            m.request({
+                method: 'PUT',
+                url: '/api/v1/page/' + self.requestedPage(),
+                data: {
+                    content: self.editor.getText().split('\n')
+                }
+            });
 
-        };
+        }.bind(self);
+
+        self.load = function() {
+            if (self.requestedPage === '') return;
+            if (self.currentPage != null && self.currentPage.title === self.requestedPage) return;
+
+            self.currentPage = new wiki.Page(self.requestedPage(), self.editor.setPage);
+        }.bind(self);
     },
     view: function(ctrl) {
-        return aceEditor.view(ctrl.editor);
+        return m('div', [
+            m('input', {onchange: m.withAttr("value", ctrl.requestedPage), value: ctrl.requestedPage()}),
+            m('button', {onclick: ctrl.load}, 'Load'),
+            m('button', {onclick: ctrl.save}, 'Save'),
+            aceEditor.view(ctrl.editor)
+        ]);
+        return
     }
 
 };
